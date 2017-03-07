f53f2aa46dab9990ed6896da7fcfee82d2da9e17
Bob Nelson <robertknelson@gmail.com>
2017-03-06 19:27:02 -0500

    ddl scripts

---
+++ eXtensibleFramework/XF.WebApi.Core/sql.scripts/schema/ddl.schemas.log.sql
@@ -0,0 +1,5 @@
+﻿/****** Object:  Schema [log]    Script Date: 3/6/2017 7:17:50 PM ******/
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'log')
+EXEC sys.sp_executesql N'CREATE SCHEMA [log]'
+
+GO
