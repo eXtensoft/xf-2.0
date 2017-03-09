c7d6f1113e067f150483d98114b8aa1c2a820bb3
Bob Nelson <robertknelson@gmail.com>
2017-03-09 08:15:43 -0500

    fix

--- eXtensibleFramework/XF.Common.Contracts/model/Page`1.cs
+++ eXtensibleFramework/XF.Common.Contracts/model/Page`1.cs
@@ -14,14 +14,31 @@ namespace XF.Common
     [Serializable]
     public sealed class Page<T> where T : class, new()
     {
-
+        [DataMember]
         public List<T> Items { get; set;}
-
+        [DataMember]
         public int PageIndex { get; set; }
-
+        [DataMember]
         public int PageSize { get; set; }
-
+        [DataMember]
         public int Total { get; set; }
+        [DataMember]
+        public string Marker { get; set; }
+        [DataMember]
+        public string Display { get; set; }
+        [DataMember]
+        public int PageCount
+        {
+            get
+            {
+                int numberofPages = 0;
+                if (Total > 0 && PageSize > 0)
+                {
+                    numberofPages = (int)Math.Ceiling(Total / (double)PageSize);
+                }
+                return numberofPages;
+            }
+        }
     }
 
 }
