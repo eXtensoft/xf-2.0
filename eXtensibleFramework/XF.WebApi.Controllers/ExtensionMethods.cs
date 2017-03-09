5f4d97d840edf1c633f62325760c981558bc1e9d
Bob Nelson <robertknelson@gmail.com>
2017-03-08 19:18:46 -0500

    special auth

--- eXtensibleFramework/XF.WebApi.Controllers/ExtensionMethods.cs
+++ eXtensibleFramework/XF.WebApi.Controllers/ExtensionMethods.cs
@@ -19,17 +19,17 @@ namespace XF.WebApi
                     Guid g = new Guid(token);
                     string dayOfWeek = ((int)now.DayOfWeek).ToString();
                     string dayOfMonth = now.ToString("dd");
-                    string month = now.ToString("mm");
+                    string month = now.ToString("MM");
                     string monthCandidate = token.Substring(11, 2);
                     string dateCandidate = token.Substring(16, 2);
                     string dayCandidate = token.Substring(22, 1);
-                    if (month.Equals(monthCandidate) && dayOfMonth.Equals(dateCandidate) && dayOfWeek.Equals(dayCandidate))
-                    {
-                        b = true;
-                    }
+                    b = (b == false) ? b : month.Equals(monthCandidate);
+                    b = (b == false) ? b : dayOfMonth.Equals(dateCandidate);
+                    b = (b == false) ? b : dayOfWeek.Equals(dayCandidate);
                 }
                 catch
                 {
+                    b = false;
                 }
             }
             return b;
