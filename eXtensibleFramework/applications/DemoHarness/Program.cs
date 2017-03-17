bb1b633acb41566b43dd80493a0d8f23df2000d6
Bob Nelson <robertknelson@gmail.com>
2017-03-17 16:11:21 -0400

    xml read

--- eXtensibleFramework/applications/DemoHarness/Program.cs
+++ eXtensibleFramework/applications/DemoHarness/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 using XF.Common;
 using XF.Common.Wcf;
 
@@ -15,7 +18,8 @@ namespace DemoHarness
         {
             Console.WriteLine("ready...");
             Console.ReadLine();
-            Execute();
+            GetSelections();
+            //Execute();
             Console.WriteLine("done...");
             Console.ReadLine();
         }
@@ -38,7 +42,27 @@ namespace DemoHarness
 
         }
 
-
+        private static void GetSelections()
+        {
+            string s = @"c:\users\princ\desktop\cyclops.data\arc.selections.xml";
+            if (File.Exists(s))
+            {
+                FileInfo info = new FileInfo(s);
+                string xml = File.ReadAllText(info.FullName);
+                XDocument xdoc = XDocument.Parse(xml);
+                var els = (from c in xdoc.Descendants("Selection") select new
+                {
+                    id = Int32.Parse(c.Element("SelectionId").Value),
+                    display = c.Element("Display").Value,
+                    token = c.Element("Token").Value,
+                    Sort = Int32.Parse(c.Element("Sort").Value),
+                    GroupId = Int32.Parse(c.Element("GroupId").Value),
+                    Icon = c.Element("Icon").Value,
+                    MasterId = Int32.Parse(c.Element("MasterId").Value)
+                }).ToDataTable();
+                //XmlDocument x =  new XmlDocument()
+            }
+        }
 
         //private static IModelRequestService GetModelService()
         //{
