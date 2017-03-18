120ce340b789f9a5767db9df1a946d0c59499ed6
Bob Nelson <robertknelson@gmail.com>
2017-03-18 19:40:08 -0400

    rpc config provider

---
+++ eXtensibleFramework/XF.Common/rpc/RpcConfigurationProvider.cs
@@ -0,0 +1,29 @@
+﻿// <copyright company="eXtensoft, LLC" file="RpcConfigurationProvider.cs">
+// Copyright © 2016 All Right Reserved
+// </copyright>
+
+namespace XF.Common.Config
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public sealed class RpcConfigurationProvider
+    {
+        #region local members
+
+        #endregion
+
+        #region properties
+
+        #endregion
+
+        #region constructors
+
+        #endregion
+
+
+    }
+
+}
