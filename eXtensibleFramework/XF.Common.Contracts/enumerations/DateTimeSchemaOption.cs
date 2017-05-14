041d21b05049f2ea342497b6910aee61498464b5
Bob Nelson <robertknelson@gmail.com>
2017-05-14 19:21:45 -0400

    schema extensions

---
+++ eXtensibleFramework/XF.Common.Contracts/enumerations/DateTimeSchemaOption.cs
@@ -0,0 +1,22 @@
+﻿// <copyright company="eXtensoft, LLC" file="DateTimeSchemaOption.cs">
+// Copyright © 2016 All Right Reserved
+// </copyright>
+
+namespace XF.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public enum DateTimeSchemaOption
+    {
+        None,
+        MonthOfYear,
+        WeekOfYear,
+        DayOfWeek,
+        DayOfYear,
+        HourOfDay,
+    }
+
+}
