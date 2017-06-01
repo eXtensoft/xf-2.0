822a9531d2f68c2e0059a2026c7c135044f39d35
Bob Nelson <robertknelson@gmail.com>
2017-06-01 11:31:56 -0400

    config fix

--- eXtensibleFramework/XF.WebApi.Common/configuration/eXtensibleWebApiElement.cs
+++ eXtensibleFramework/XF.WebApi.Common/configuration/eXtensibleWebApiElement.cs
@@ -63,21 +63,6 @@ namespace XF.WebApi.Config
             set { this["datastoreKey"] = value; }
         }
 
-        [ConfigurationProperty("loggingSchema", IsRequired = false)]
-        public string LoggingSchema
-        {
-            get
-            {
-                object o = this["loggingSchema"];
-                string s = o != null ? o.ToString() : DateTimeSchemaOption.None.ToString();
-                DateTimeSchemaOption option;
-                Enum.TryParse<DateTimeSchemaOption>(s, true, out option);
-                return option.ToString();
-            }
-            set { this["loggingSchema"] = value; }
-        }
-
-
 
     }
 
