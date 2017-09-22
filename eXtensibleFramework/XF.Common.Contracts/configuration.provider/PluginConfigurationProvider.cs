9239d4a5f9367cf7d422b51ff3f86bb24efc393b
robert nelson <robertknelson@gmail.com>
2017-09-22 07:39:26 -0400

    plugin configuration provider

---
+++ eXtensibleFramework/XF.Common.Contracts/configuration.provider/PluginConfigurationProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF.Common.Contracts
+{
+    public abstract class PluginConfigurationProvider : SystemConfigurationProvider
+    {
+
+    }
+}
