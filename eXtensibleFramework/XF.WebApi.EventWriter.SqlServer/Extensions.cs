6e2e30c01f6034b2329dfd91df206e4dc20c5723
robert nelson <robertknelson@gmail.com>
2017-09-24 15:45:20 -0400

    event writer (sqlserver)

---
+++ eXtensibleFramework/XF.WebApi.EventWriter.SqlServer/Extensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XF.Common;
+
+namespace XF.WebApi.EventWriter.SqlServer
+{
+    public static class Extensions
+    {
+        public static string ToSchema(this DateTime now, DateTimeSchemaOption option, string defaultSchema = "log")
+        {
+            string s = !String.IsNullOrWhiteSpace(defaultSchema) ? defaultSchema.Trim() : "log";
+
+            switch (option)
+            {
+                case DateTimeSchemaOption.MonthOfYear:
+                    s = now.Date.ToString("MMM");
+                    break;
+                case DateTimeSchemaOption.WeekOfYear:
+                    s = now.Date.WeekOfYear().ToString("000");
+                    break;
+                case DateTimeSchemaOption.DayOfWeek:
+                    s = now.DayOfWeek.ToString().Substring(0, 3);
+                    break;
+                case DateTimeSchemaOption.DayOfYear:
+                    s = now.DayOfYear.ToString("000");
+                    break;
+                case DateTimeSchemaOption.HourOfDay:
+                    s = now.ToString("HH");
+                    break;
+                case DateTimeSchemaOption.HourOfDayOfWeek:
+                    s = String.Format("{0}{1}", now.ToString("HH"), (int)now.DayOfWeek);
+                    break;
+                case DateTimeSchemaOption.None:
+                default:
+                    break;
+            }
+
+            return s.ToLower();
+        }
+        public static int WeekOfYear(this DateTime now)
+        {
+            int day = (int)now.DayOfWeek;
+            if (--day < 0)
+            {
+                day = 6;
+            }
+            int weekNumber = (now.AddDays(3 - day).DayOfYear - 1) / 7 + 1;
+            return weekNumber;
+        }
+    }
+
+}
